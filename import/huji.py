922e8100f5632300edcf9fb5103872083363f259
dixiaotao <xtdi@outlook.com>
2021-01-11 19:40:13 +0800

    dxt

--- import/huji.py
+++ import/huji.py
@@ -1,59 +1,69 @@
 import openpyxl
 import time
 import os
+from mariadb.mariadb_manager import MariadbManager
 
 
-def parse_huji_excel(excel_full_path):
+def parse_huji_excel(excel_full_path, mariadb_conn, insert_sql_stmt):
 
     excel_book = openpyxl.load_workbook(excel_full_path, read_only=True)
     excel_sheet = excel_book.worksheets[0]
 
     print("总行数：" + str(excel_sheet.max_row))
     print("总列数：" + str(excel_sheet.max_column))
-    insert_sql_pre = "INSERT INTO HUJI (STAT_TIME,STAT_NO,BIRTH_DATE,SEX,ID_NO,NMAE,PHONE,ADDRESS) VALUES ("
+
+    insert_sql_stmt = "INSERT INTO HUJI (STAT_TIME, STAT_NO, BIRTH_DATE, SEX, ID_NO, NMAE, PHONE, ADDRESS)"
+    insert_sql_stmt = insert_sql_stmt + " VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
     datarow_list = []
     row_num = 0
     for cur_row in excel_sheet.rows:
         row_num = row_num + 1
-        item_value = "\""
-        # cur_row[1]  统计时间
-        temp_stat_time = cur_row[1].value.strip()
-        item_value = item_value + temp_stat_time
-        item_value = item_value + "\",\""
-        # cur_row[3]  编码编号
-        temp_stat_no = cur_row[3].value.strip()
-        item_value = item_value + temp_stat_no
-        item_value = item_value + "\",\""
-        # cur_row[4]  出生日期
-        temp_birth_date = cur_row[4].value.strip()
-        item_value = item_value + temp_birth_date
-        item_value = item_value + "\",\""
-        # cur_row[5]  性别
-        temp_sex = cur_row[5].value.strip()
-        item_value = item_value + temp_sex
-        item_value = item_value + "\",\""
-        # cur_row[6] 身份证号
-        temp_id_no = cur_row[6].value.strip()
-        item_value = item_value + temp_id_no
-        item_value = item_value + "\",\""
-        # cur_row[7] 姓名
-        temp_name = cur_row[7].value.strip()
-        item_value = item_value + temp_name
-        item_value = item_value + "\",\""
-        # cur_row[8] 电话号码
-        temp_phone = cur_row[8].value.strip()
-        item_value = item_value + temp_phone
-        item_value = item_value + "\",\""
-        # cur_row[9] 地址
-        temp_address = cur_row[9].value.strip()
-        item_value = item_value + temp_address
-        item_value = item_value + "\"）"
+        temp_stat_time = cur_row[2].value.strip()   # cur_row[2]  统计时间
+        temp_stat_no = cur_row[4].value.strip()     # cur_row[4]  编码编号
+        temp_birth_date = cur_row[5].value.strip()  # cur_row[5]  出生日期
+        temp_sex = cur_row[6].value.strip()         # cur_row[6]  性别
+        temp_id_no = cur_row[7].value.strip()       # cur_row[7]  身份证号
+        temp_name = cur_row[8].value.strip()        # cur_row[8]  姓名
+        temp_phone = cur_row[9].value.strip()       # cur_row[9]  电话号码
+        temp_address = cur_row[10].value.strip()     # cur_row[10]  地址
 
-        mysql_values_tuple = (temp_stat_time,temp_stat_no, temp_birth_date,
+        if len(temp_phone) == 0:
+            print("用户的电话号码为空，本条数据跳过" % temp_name)
+            continue
+
+        mysql_values_tuple = (temp_stat_time, temp_stat_no, temp_birth_date,
                               temp_sex, temp_id_no, temp_name, temp_phone, temp_address)
         datarow_list.append(mysql_values_tuple)
-        # print( insert_sql_pre + item_value )
-    print("文件入库完成，共入库数据"+str(0)+"条")
+        if len(datarow_list) == 10000:
+            temp_num = batch_insert_data(mariadb_conn, insert_sql_stmt, datarow_list)
+            inserted_num = inserted_num + temp_num
+            datarow_list.clear()
+            print("共计插入数据%d行" % inserted_num)
+
+    if len(datarow_list) > 0:
+        temp_count = batch_insert_data(mariadb_conn, insert_sql_stmt, datarow_list)
+        inserted_num = inserted_num + temp_count
+        print("共计插入数据%d行" % inserted_num)
+    print("文件入库完成，共入库数据"+str(inserted_num)+"行")
+
+
+def batch_insert_data(mariadb_conn, sql_stmt, values_list):
+
+    begin_time = time.time()
+    table_cursor = mariadb_conn.cursor()
+    total_count = 0
+    try:
+        total_count = table_cursor.executemany(sql_stmt, values_list)
+        mariadb_conn.commit()
+    except Exception as ex:
+        print(ex)
+        mariadb_conn.rollback()
+    table_cursor.close()
+    end_time = time.time()
+    spend_time = end_time-begin_time
+    print("本次插入数据%d条" % total_count+"  共计花费时间:%s秒" % format(spend_time, '0.2f'))
+    return total_count
+
 
 def check_huji_file_format(full_path, column_header_list):
 
@@ -79,15 +89,24 @@ def check_huji_file_format(full_path, column_header_list):
         else:
             print("文件:" + full_path + " 的格式无误")
 
+        temp_id_no = excel_sheet.cell(2, 7).value.strip()  # cur_row[6]  身份证号
+        temp_name = excel_sheet.cell(2, 8).value.strip()  # cur_row[7]  姓名
+        temp_phone = excel_sheet.cell(2, 9).value.strip()  # cur_row[8]  电话号码
+        if len(temp_id_no) != 15:
+            if len(temp_id_no) != 18:
+                print("身份证格式有误:%s" %temp_id_no)
+
     else:
         print("文件:" + full_path + " 的行数为空，请检查！")
 
 
+def parse_all_huji_data():
 
-def parse_huji_data():
+    mariadb_manager = MariadbManager("127.0.0.1", 3306, "privacydata", "root", "pmo@2016",  charset="utf8mb4")
+    mariadb_manager.open_connect()
 
     column_header = ["所属户籍站", "统计时间", "居住地址", "编码编号", "出生日期", "性别", "身份证", "名字", "手机号", "地址", "编码", "编码"]
-    rootdir = r"D:\Downloads\户籍数据1.03GB"
+    rootdir = r"D:\downloads\privacydata\huji"
     list = os.listdir(rootdir)  # 列出文件夹下所有的目录与文件
     file_num = 0
     for i in range(0, len(list)):
@@ -95,14 +114,14 @@ def parse_huji_data():
         if os.path.isfile(file_path):
             file_num = file_num + 1
             print("开始第"+ str(file_num) + "文件:" + file_path + "！")
-            # check_huji_file_format(file_path, column_header)
-            parse_huji_excel(file_path)
+            check_huji_file_format(file_path, column_header)
+            # parse_huji_excel(file_path)
             print()
 
 def main():
 
     try:
-        parse_huji_data()
+        parse_all_huji_data()
     except Exception as ex:
         print(ex)
 
