24f7d8932db2977326acf3e02902e7a4e1549856
dixiaotao <xtdi@outlook.com>
2021-01-22 19:47:41 +0800

    dxt

--- import/tianya.py
+++ import/tianya.py
@@ -177,10 +177,95 @@ def parse_all_files():
                 logfile.write(write_info + "\n" )
 
 
+def parse_invalid_records():
+
+    # mariadb_manager = MariadbManager("127.0.0.1", 3306, "privacydata", "root", "pmo@2016",  charset="utf8mb4")
+    mariadb_manager = MariadbManager("192.168.1.116", 3308, "privacydata", "root", "Springdawn@2016", charset="utf8mb4")
+    mariadb_manager.open_connect()
+    insert_sql_stmt = "INSERT INTO tianya (login_name, password, email)"
+    insert_sql_stmt = insert_sql_stmt + " VALUES (%s, %s, %s)"
+
+    records_file_path = r"D:\tianya_invalid_format-dxt.txt"
+    invalid_records_file_path = r"D:\tianya_new_invalid.txt"
+    with open(records_file_path, "r", encoding="utf8") as file_handle:
+        total_rows = 0
+        inserted_num = 0
+        datarow_list = []
+        blank_rows_num = 0
+        invalid_format_rows_list = []
+        invalid_format_rows_num = 0
+        while True:
+            try:
+                total_rows = total_rows + 1
+
+                current_row = file_handle.readline()
+                if not current_row:
+                    temp_count = batch_insert_data(mariadb_manager.connect, insert_sql_stmt, datarow_list)
+                    inserted_num = inserted_num + temp_count
+                    datarow_list.clear()
+                    # print("文件读取完成，共计读取数据%d行" % (total_rows-1)  + ",其中：插入数据：%d行" % inserted_num)
+                    # print("格式无效数据:%d行" % invalid_format_rows_num)
+                    # print("空数据:%d行" % blank_rows_num)
+
+                    with open(invalid_records_file_path, "w", encoding="utf8") as f:
+                        f.writelines(invalid_format_rows_list)
+                        invalid_format_rows_list.clear()
+                    break
+
+                current_row = current_row.strip()
+                if len(current_row) == 0:
+                    blank_rows_num = blank_rows_num + 1
+                    continue
+
+                item_list = []
+                split_char = " "
+                temp_all_list = current_row.split(split_char)
+                for i in range(len(temp_all_list)):
+                    if len(temp_all_list[i].strip()) > 0:
+                        item_list.append(temp_all_list[i].strip())
+                if len(item_list) < 2:
+                    # print("数据项不足三项："+current_row)
+                    invalid_format_rows_num = invalid_format_rows_num + 1
+                    invalid_format_rows_list.append(current_row + "\n")
+                    continue
+
+                temp_login_name = item_list[0].strip()
+                temp_password = ""
+                temp_email = item_list[len(item_list)-1].strip()
+
+                if temp_email.find("@") == -1:
+                    invalid_format_rows_num = invalid_format_rows_num + 1
+                    invalid_format_rows_list.append(current_row + "\n")
+                    continue
+
+                for i in range(len(item_list)):
+                    if i == 0 or i == len(item_list) - 1:
+                        continue
+                    else:
+                        temp_password = temp_password + item_list[i]
+
+                temp_password = temp_password.replace(" ","")
+                mysql_values_tuple = (temp_login_name, temp_password, temp_email)
+                datarow_list.append(mysql_values_tuple)
+                if len(datarow_list) == 10000:
+                    temp_num = batch_insert_data(mariadb_manager.connect, insert_sql_stmt, datarow_list)
+                    inserted_num = inserted_num + temp_num
+                    datarow_list.clear()
+                    # print("共计插入数据%d行" % inserted_num)
+            except Exception as ex:
+                print(ex)
+                print("程序异常退出，已经插入数据%d行" % inserted_num)
+                break
+
+    return_result_str = "文件共计:" + str(total_rows - 1) + "行，其中插入数据:" + str(inserted_num)
+    return_result_str = return_result_str + "行，格式无效数据:" + str(invalid_format_rows_num)
+    return_result_str = return_result_str + "行，空数据:" + str(blank_rows_num) + "行"
+    return return_result_str
+
 def main():
 
     try:
-        parse_all_files()
+        parse_invalid_records()
     except Exception as ex:
         print(ex)
 
